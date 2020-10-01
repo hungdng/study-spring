e4864886ccf56530fa7a4baa2dbfc272778c82ec
HUNG\HUNG <hungdng.92@gmail.com>
2020-10-01 10:52:47 +0700

    Add Base Entity

---
+++ study-spring-data/src/main/java/hung/tran/studyspring/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package hung.tran.studyspring.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
